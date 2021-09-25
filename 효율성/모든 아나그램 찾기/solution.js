10ee24f659e303aa1c3364d5c86e5a8285c1121f
ahnella04 <chqkq96@gmail.com>
2021-09-26 00:49:01 +0900

    jsAlgorithm

--- 효율성/모든 아나그램 찾기/solution.js
+++ 효율성/모든 아나그램 찾기/solution.js
@@ -1,7 +1,11 @@
 function compareMaps(map1, map2) {
     // console.log(map1.size); b a c 순서
     if (map1.size !== map2.size) return false;
-    
+    for (let [key, val] of map1) {
+        if (!map2.has(key)) return false;
+        if (map2.get(key) !== val) return false; 
+    }
+    return true;
 }
 
 function solution(s, t) {

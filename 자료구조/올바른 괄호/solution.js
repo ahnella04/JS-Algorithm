4ea14433424dc10839b5ba59102f4e6cbf90d8bd
ahnella04 <chqkq96@gmail.com>
2021-09-26 22:38:06 +0900

    jsAlgorithm

---
+++ 자료구조/올바른 괄호/solution.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    let answer;
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(s[i]);
+        } else {
+            stack.pop();
+        }
+    }
+
+    if (stack.length === 0) {
+        answer = "YES"
+    } else {
+        answer = "NO"
+    }
+    
+    return answer;
+}
+
+let str = "(()(()))(()";
+console.log(solution(str))
